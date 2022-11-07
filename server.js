c276f75370aed258c5a5bb0edccf77e716e1f3bc
inthedownpour <simjiyeon@naver.com>
2022-11-08 00:58:21 +0900

    cors 오류 test

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const corsOptions = {
     },
   };
 
-app.use(cors(corsOptions));
+//보안 취약하지만 그냥 모든 주소를 허용 하는 걸로 오류 확인 
+app.use(cors());
+//app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
