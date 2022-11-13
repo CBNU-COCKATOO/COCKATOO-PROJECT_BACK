c5c6edbff6d3817aa19d6ddb470dba933bd90576
inthedownpour <simjiyeon@naver.com>
2022-11-13 18:48:33 +0900

    검색 res에 코디, 옷 사용자 정보 추가

--- app/models/search.model.js
+++ app/models/search.model.js
@@ -4,10 +4,10 @@ exports.findByNAME = (name, result) => {
     var sql1 = 'SELECT u_id, u_name, u_image, u_mainst, u_height, u_weight FROM USER WHERE u_name LIKE ?;';
     var sql1s = sql.format(sql1, '%'+name+'%');
 
-    var sql2 = 'SELECT clo_id, clo_name, clo_image, clo_style, clo_size FROM clothes WHERE clo_name LIKE ?;';
+    var sql2 = 'WITH clo_info AS(SELECT clo.clo_id, clo.clo_name, clo.clo_image, clo.clo_style, clo.clo_size, clo.u_id FROM clothes clo WHERE clo_name LIKE ?) SELECT u.u_id, c.clo_name, c.clo_image, c.clo_style, c.clo_size FROM clo_info c, USER u WHERE u.u_id = c.u_id;';
     var sql2s = sql.format(sql2, '%'+name+'%');
 
-    var sql3 = 'SELECT cody_id, cody_name, cody_style, cody_image FROM cody WHERE cody_name LIKE ?;';
+    var sql3 = 'WITH cody_info AS (SELECT cd.cody_id, cd.cody_name, cd.cody_image, cd.cody_style, cd.u_id FROM cody cd WHERE cody_name LIKE ?) SELECT u.u_id, d.cody_name, d.cody_image FROM cody_info d, USER u WHERE u.u_id = d.u_id;';
     var sql3s = sql.format(sql3, '%'+name+'%');
 
     sql.query(sql1s + sql3s + sql2s, (err, res) => {
