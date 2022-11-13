2a6d01f7987d62086e4fd58b10f6382bf8bbcc3e
inthedownpour <simjiyeon@naver.com>
2022-11-13 23:56:42 +0900

    follow/unfollow 한 URI로 가능하게 수정

--- app/routes/follow.routes.js
+++ app/routes/follow.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const follow = require("../controller/follow.controller.js");
     
     app.post("/follow", follow.follow);
-    app.post("/unfollow", follow.unfollow);
+    //app.post("/unfollow", follow.unfollow);
     app.get("/followuser/:userId", follow.follow_closet);
 
 };
