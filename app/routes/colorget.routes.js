37a9e5853ecf75fff475c22fb7598e1c5b02074a
inthedownpour <simjiyeon@naver.com>
2022-11-17 04:29:43 +0900

    모듈 테스트2

--- app/routes/colorget.routes.js
+++ app/routes/colorget.routes.js
@@ -1,19 +1,19 @@
-module.exports =app =>{
-    const ColorThief = require('colorthief');
+// module.exports =app =>{
+//     const ColorThief = require('colorthief');
 
-    app.post("/getcolor", (req, res)=>{
-        const img = req.body.clo_image;
+//     app.post("/getcolor", (req, res)=>{
+//         const img = req.body.clo_image;
 
-        ColorThief.getColor(img)
-            .then(color => { 
-                res.json(color)
-             })
-            .catch(err => { 
-                console.log(err);
-                res.json({
-                    message: "이미지 색상 추출에 실패했습니다."
-                })
+//         ColorThief.getColor(img)
+//             .then(color => { 
+//                 res.json(color)
+//              })
+//             .catch(err => { 
+//                 console.log(err);
+//                 res.json({
+//                     message: "이미지 색상 추출에 실패했습니다."
+//                 })
             
-            })
-    })
-}
+//             })
+//     })
+// }
