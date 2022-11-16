ab5d1f662e61472b078084e07babdbda65e8f627
inthedownpour <simjiyeon@naver.com>
2022-11-16 23:10:12 +0900

    index 도감 생성 시 else문 추가

--- app/models/dictionary.model.js
+++ app/models/dictionary.model.js
@@ -15,8 +15,7 @@ Dict.create = (userId, dict, result) => {
             console.log("error: ", err);
             result(err, null);
             return;
-        }
-        console.log("Created dict_index: ", dict.d_index);
+        }else console.log("Created dict_index: ", dict.d_index);
     });
     result(null, {kind:"created"});
 
