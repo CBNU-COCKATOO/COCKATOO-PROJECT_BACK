919a88b646a1774a059347e710badacfbbe7f20f
inthedownpour <simjiyeon@naver.com>
2022-11-16 23:22:21 +0900

    req.body 확인 2

--- app/controller/dictionary.controller.js
+++ app/controller/dictionary.controller.js
@@ -22,11 +22,13 @@ exports.CreateIndex = (req, res) =>{
         message: "Content can not be empty!"
     });
   }else{
+    console.log("컨트롤 쪽 받기 req.body.d_index: "+req.body.d_index);
+    console.log("req.body.d_image:"+req.body.d_image);
     const dict = new Dict({
       d_index : req.body.d_index,
       d_image : req.body.d_image
       });
-      console.log(req.body.d_index);
+      
     
       Dict.create(req.params.userId, dict, (err,data)=>{
         if (err) {
