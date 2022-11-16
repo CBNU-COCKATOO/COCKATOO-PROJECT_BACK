06b56307dae4bd6dd1da5e8ab97b5960a8ca0c44
inthedownpour <simjiyeon@naver.com>
2022-11-17 01:36:16 +0900

    이메일 인증 코드 uri 변경

--- app/routes/email.routes.js
+++ app/routes/email.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
     const email = require("../controller/email.controller.js");
     app.post("/emailauth", email.EmailSend);
-    app.get("/emailauth", email.EmailCheck);
+    app.post("/emailauthcode", email.EmailCheck);
 };
